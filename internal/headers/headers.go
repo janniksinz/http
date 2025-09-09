616a37ee9450e6906b20ff35bc18b702510d2eae
janniksinz <j.sinz@gmx.de>
2025-09-10 00:00:22 +0200

    feat: header parsing

---
+++ internal/headers/headers.go
@@ -0,0 +1,58 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Headers map[string]string
+
+var rn = []byte("\r\n")
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func (h Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+
+	for {
+		idx := bytes.Index(data[read:], rn)
+		if idx == -1 {
+			break
+		}
+
+		// empty headers
+		if idx == 0 {
+			done = true
+			read += len(rn)
+			break
+		}
+
+		name, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+
+		read += idx + len(rn)
+		h[name] = value
+	}
+
+	return read, done, nil
+}
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed header")
+	}
+
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1]) // remove optional whitespace
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("malformed field name")
+	}
+
+	return string(name), string(value), nil
+}
