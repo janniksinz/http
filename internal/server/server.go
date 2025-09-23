f6b8ff1348006469a3ee7ac894ce375ac9f6c077
janniksinz <j.sinz@gmx.de>
2025-09-24 00:30:26 +0200

    feat: httpServer running

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,24 +1,61 @@
 package server
 
 import (
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"time"
 )
 
-const port = 42069
+type Server struct {
+	state  string
+	closed bool
+}
 
-func main() {
-	server, err := server.Serve(port)
+func runConnection(s *Server, conn io.ReadWriteCloser) {
+	defer conn.Close()
+	slog.Info("runConnection#")
+	out := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!")
+	_, err := conn.Write(out)
 	if err != nil {
-		log.Fatalf("Error starting server: %v", err)
+		slog.Error("write error", "error", err)
 	}
-	defer server.Close()
-	log.Println("Server started on port", port)
+	slog.Info("Return", "Res:", string(out))
+	time.Sleep(10 * time.Millisecond)
+}
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	<-sigChan
-	log.Println("Server gracefully stopped")
+func runServer(s *Server, listener net.Listener) { // go routine doesn't need error
+	slog.Info("runServer#")
+
+	// listener
+	for {
+		conn, err := listener.Accept()
+		if s.closed {
+			return
+		}
+		if err != nil {
+			return
+		}
+
+		go runConnection(s, conn) // go routine to
+	}
+
+}
+
+func Serve(port uint16) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	server := &Server{closed: false}
+	go runServer(server, listener) // go routine
+
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed = true
+	return nil
 }
