85a5485e8a27ce198618295eae00d68f7f6ff910
janniksinz <j.sinz@gmx.de>
2025-09-25 00:52:01 +0200

    feat: add response codes

---
+++ internal/response/response.go
@@ -0,0 +1,12 @@
+package response
+
+type Response struct {
+}
+
+type StatusCode int
+
+const (
+	StatusOK                  StatusCode = 200
+	StatusBadRequest          StatusCode = 400
+	StatusInternalServerError StatusCode = 500
+)
